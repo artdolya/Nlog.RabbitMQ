ac4c819ca083275d3460c5bf6a3fda89f54ab82f
Artem <adolya@users.noreply.github.com>
2022-11-02 12:23:27 +0200

    Applied formatting (#52) #minor
    Co-authored-by: Artem Dolya <artem.dolya@schibtsed.com>

--- src/Nlog.RabbitMQ.Target/Field.cs
+++ src/Nlog.RabbitMQ.Target/Field.cs
@@ -3,50 +3,50 @@ using NLog.Layouts;
 
 namespace Nlog.RabbitMQ.Target
 {
-	[NLogConfigurationItem]
-	[ThreadAgnostic]
-	public class Field
-	{
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Field" /> class.
-		/// </summary>
-		public Field()
-			: this(null, null, null)
-		{
-		}
+    [NLogConfigurationItem]
+    [ThreadAgnostic]
+    public class Field
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Field" /> class.
+        /// </summary>
+        public Field()
+            : this(null, null, null)
+        {
+        }
 
-		/// <summary>
-		/// Initializes a new instance of the <see cref="Field" /> class.
-		/// </summary>
-		/// <param name="key">The field key</param>
-		/// <param name="name">The field display name</param>
-		/// <param name="layout">The field layout</param>
-		public Field(string key, string name, Layout layout)
-		{
-			this.Key = key;
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Field" /> class.
+        /// </summary>
+        /// <param name="key">The field key</param>
+        /// <param name="name">The field display name</param>
+        /// <param name="layout">The field layout</param>
+        public Field(string key, string name, Layout layout)
+        {
+            this.Key = key;
 
-			if (string.IsNullOrEmpty(name))
-				name = key;
+            if (string.IsNullOrEmpty(name))
+                name = key;
 
-			this.Name = name;
-			this.Layout = layout;
-		}
+            this.Name = name;
+            this.Layout = layout;
+        }
 
-		/// <summary>
-		/// Gets or sets the name of the field
-		/// </summary>
-		public string Name { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the field
+        /// </summary>
+        public string Name { get; set; }
 
-		/// <summary>
-		/// Gets or sets the key of the field
-		/// </summary>
-		[RequiredParameter]
-		public string Key { get; set; }
+        /// <summary>
+        /// Gets or sets the key of the field
+        /// </summary>
+        [RequiredParameter]
+        public string Key { get; set; }
 
-		/// <summary>
-		/// Gets or sets the layout of the field
-		/// </summary>
-		[RequiredParameter]
-		public Layout Layout { get; set; }
-	}
+        /// <summary>
+        /// Gets or sets the layout of the field
+        /// </summary>
+        [RequiredParameter]
+        public Layout Layout { get; set; }
+    }
 }
