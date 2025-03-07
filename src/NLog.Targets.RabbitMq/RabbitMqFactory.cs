11cd9e22e8e2aa31d070e1c85da0a21100069736
Artem Dolya <artem.dolya@gmail.com>
2025-03-07 16:35:04 -0500

    Fix for build

---
+++ src/NLog.Targets.RabbitMq/RabbitMqFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using RabbitMQ.Client;
+
+namespace NLog.Targets.RabbitMq;
+
+public class RabbitMqFactory
+{
+    private readonly ConnectionFactory _factory;
+    private readonly IEnumerable<AmqpTcpEndpoint> _endpoints;
+
+    public RabbitMqFactory(ConnectionFactory factory, IEnumerable<AmqpTcpEndpoint> endpoints)
+    {
+        _factory = factory;
+        _endpoints = endpoints;
+    }
+
+    public async Task<IConnection> CreateConnectionAsync()
+    {
+        if (!_endpoints.Any() && _factory.Uri == null)
+        {
+            throw new ArgumentException("No endpoints or uri provided");
+        }
+        
+        return _endpoints.Any() ? await  _factory.CreateConnectionAsync(_endpoints) : await _factory.CreateConnectionAsync();
+    }
+}
