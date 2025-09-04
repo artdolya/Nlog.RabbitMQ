eaf59aa852fd1ae81fab4f54189cde776fca877e
Paweł Wołoszyn <56560480+pawelwoloszyn-dev@users.noreply.github.com>
2025-09-04 22:25:48 +0200

    Updated usings in LogLine

--- src/Nlog.RabbitMQ.Target/LogLine.cs
+++ src/Nlog.RabbitMQ.Target/LogLine.cs
@@ -1,5 +1,6 @@
-﻿using System.Collections.Generic;
-using System.Text.Json.Serialization;
+using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace Nlog.RabbitMQ.Target
 {
